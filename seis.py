b16a8258d962ff54e04b9cb4c6072bfba13d6adc
Nicholas Pelham <pelhamnicholas@gmail.com>
2016-12-22 08:01:01 +0000

    workable ascii output

--- seis.py
+++ seis.py
@@ -10,25 +10,25 @@ MULTIPLIER = 0.015625;
 # Seismograph members
 sourceName = "AmySeis"
 samplesPerSecond = 20
-samplesPerFile = 86400 * samplesPerSecond
+samplesPerFile = 86400 / 24 * samplesPerSecond
 timeStamp = datetime.datetime.now().isoformat()
 format = "SLIST"
 type = "FLOAT"
-units = "mV"
+units = "COUNT"
 
 fileName = sourceName + "_" + timeStamp
 
 # Open file to output to
 file = open(fileName + ".txt", "w+")
-file.write("TIMESERIES %s %s %s %s %s %s %s\n" % (sourceName, samplesPerFile, samplesPerSecond, timeStamp, format, type, units))
+file.write("TIMESERIES _Amy__Z_, %s samples, %s sps, %s, %s, %s, %s\n" % (samplesPerFile, samplesPerSecond, timeStamp, format, type, units))
 file.close()
 
-for i in range(0, 200): # one day 288000
+for i in range(0, samplesPerFile / 6): # one day 288000
 	file = open(fileName + ".txt", "a+")
 
 	for j in range(0, 6):
 		value = adc.read_adc_difference(0, gain=GAIN)
-		file.write("\t" + '{0}'.format(value*MULTIPLIER))
+		file.write("\t" + '{0}'.format(value))
 		time.sleep(0.05)
 
 	file.write("\n")
