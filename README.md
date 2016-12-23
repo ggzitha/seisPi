8f042debc4b9618801b5f37d48ca0372287f600a
Nicholas Pelham <pelhamnicholas@gmail.com>
2016-12-23 02:30:37 +0000

    split readSeis to hour and day

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
+install obsPy:
+
+add to /etc/apt/sources.list
+deb http://deb.obspy.org jessie main
+get the key
+wget --quiet -O - https://raw.github.com/obspy/obspy/master/misc/debian/public.key | sudo apt-key add -
+install the program
+sudo apt-get install python-obspy python3-obspy
+
+enable i2c:
+
+sudo apt-get install python-smbus i2c-tools
+
+add to /etc/modules
+i2c-bcm2708
+i2c-dev
+
+add to /boot/config.txt
+dtparam=i2c_arm=on
+
+
+install ADS1x15:
+
+sudo pip install adafruit-ads1x15
+
+
 Add to crontab:
 
 crontab -e
--- readHour.py
+++ readHour.py
@@ -6,30 +6,22 @@ import sys, getopt, os
 
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv, "hio:") 
+		opts, args = getopt.getopt(argv, "h:io") 
 	except getopt.GetoptError:
 		print 'readSeis_ASCII.py [-i <inputfile>] [-o <outputfile>]'
 		sys.exit(2)
 	date = args[0]
-	infile = "*"
 	outfile = "data/mseed/" + date + ".mseed"
 	for opt, arg in opts:
 		if opt == '-h':
 			print 'readSeis_ASCII.py [-i <inputfile>] [-o <outputfile>]'
-		elif opt in ("-o", "--outfile"):
-			outfile = "mseed/" + arg + ".mseed"
 		elif opt in ("-i", "--ifile"):
-			infile = arg + ".txt"
+			infile = "data/" + arg + ".txt"
 			outfile = outfile + "-" + arg
 
-	# Creating the MSEED format file to plot
-	if not os.path.exists("data/mseed/"):
-		os.makedirs("data/mseed/")
-	call(["./ascii2mseed -o " + outfile + " data/" + date + "/" + infile], shell=True)
-
 	# Plotting
-	data = read(outfile)
-	data.plot(type='dayplot')
+	data = read(infile)
+	data.plot()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
