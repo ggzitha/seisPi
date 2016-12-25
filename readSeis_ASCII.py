f76555e5ee8dc96b00c6e285a8bb2953723b7798
Nicholas Pelham <pelhamnicholas@gmail.com>
2016-12-25 12:54:01 -0800

    lowpass filter added

--- readSeis_ASCII.py
+++ readSeis_ASCII.py
@@ -6,11 +6,12 @@ import sys, getopt, os, obspy
 
 def main(argv):
 	try:
-		opts, args = getopt.getopt(argv, "h", ["hour="]) 
+		opts, args = getopt.getopt(argv, "hs", ["hour=", "scale="]) 
 	except getopt.GetoptError:
 		print 'readSeis_ASCII.py [-o <outputfile>] [--hour=<HH>] <YYYY-MM-DD>'
 		sys.exit(2)
 	date = args[0]
+	scale = 500
 	infile = "*"
 	outfile = "data/mseed/" + date + ".mseed"
 	for opt, arg in opts:
@@ -18,6 +19,8 @@ def main(argv):
 			print 'readSeis_ASCII.py [-o <outputfile>] [--hour=<HH>] <YYYY-MM-DD>'
 		elif opt in ("-o", "--outfile"):
 			outfile = "data/mseed/" + arg + ".mseed"
+		elif opt in ("-s", "--scale"):
+			scale = int(arg)
 		elif opt in ("--hour"):
 			infile = arg + ".txt"
 			data = read(("data/" + date + "/" + infile))
@@ -33,7 +36,7 @@ def main(argv):
 	# Plotting
 	data = read(outfile)
 	data.filter('lowpass', freq=1.0, corners=4, zerophase=True)
-	data.plot(type='dayplot', vertical_scaling_range=5000)
+	data.plot(type='dayplot', vertical_scaling_range= scale)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
